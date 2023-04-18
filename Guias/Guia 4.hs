071edf389b0199b614bffaaa3a98c6ac39d13b40
SantiagoRocca <42080273+Wiecheversplash@users.noreply.github.com>
2023-04-17 22:58:54 -0300

    Hasta ejercicio 7

--- Guias/Guia 4.hs
+++ Guias/Guia 4.hs
@@ -6,9 +6,11 @@ fibonacci 1 = 1
 fibonacci x = fibonacci (x-2) + fibonacci (x-1)
 
 --2)
+--floor x  funcion que devuelve el piso del numero
 
 parteEntera :: Float -> Integer
-parteEntera x =  toInteger (round x)
+parteEntera x | x<1 = 0
+              | otherwise = 1 + parteEntera (x-1)
 
 
 --3)
@@ -22,5 +24,34 @@ esDivisible x y | x==y = True
 
 sumaImpares :: Integer -> Integer
 sumaImpares x | x==1 = 1
-              | x==2 = 3
               | otherwise = 2*x -1 + sumaImpares (x-1)
+
+--5)
+
+medioFact :: Integer -> Integer 
+medioFact x | x==0 || x==1 = 1
+            | otherwise = x * medioFact(x-2)
+
+--6)
+
+sumaDigitos :: Integer -> Integer 
+sumaDigitos x | x==0 || x<0 = 0 
+              | otherwise = mod x 10 + sumaDigitos(div x 10)
+
+--7)
+
+todosDigitosIguales :: Int -> Bool
+todosDigitosIguales x | x<10 = True
+                      | x==0 = True
+                      | auxSumaDigitos x==(auxContador x) * (auxdigitoUnidades x) = True
+                      | otherwise = False
+ 
+auxdigitoUnidades :: Int -> Int
+auxdigitoUnidades x | x>0 = (mod x 10)
+
+auxSumaDigitos :: Int -> Int
+auxSumaDigitos x | x==0 || x<0 = 0 
+                 | otherwise = mod x 10 + auxSumaDigitos(div x 10)
+
+auxContador :: Int-> Int
+auxContador x = length (show (abs x))
