92e0b122f38de2a52130d0bd6d5421931175741d
Santiago <Santiagorocca17@gmail.com>
2023-04-22 22:05:46 -0300

    update

--- CMS/1/1) sumaMenosQueMax/sumaMenosQueMax.hs
+++ CMS/1/1) sumaMenosQueMax/sumaMenosQueMax.hs
@@ -1,14 +1,12 @@
 -- No editar esta parte
---main :: IO ()
---main = do
- -- x <- readLn
-  --print (sumaMenosQueMax (x :: (Int, Int, Int)))
+main :: IO ()
+main = do
+  x <- readLn
+  print (sumaMenosQueMax (x :: (Int, Int, Int)))
 
 sumaMenosQueMax :: (Int, Int, Int) -> Bool
 sumaMenosQueMax (x,y,z) | maxTripla (x,y,z) > (minTripla (x,y,z) + medioTripla (x,y,z)) = True
                         | otherwise = False
-
--- Completar acá la definición de la función
 
 maxTripla  :: (Int,Int,Int) -> Int
 maxTripla  (x,y,z) | x>=y && x>=z = x
@@ -20,10 +18,7 @@ minTripla  (x,y,z) | x<=y && x<=z = x
                    | y<=x && y<=z = y
                    | otherwise = z
 
-medioTripla :: (Int, Int,Int) -> Int
-medioTripla (x,y,z) | (x>=y && x<=z) || (x<=y && x<=z) = x
-                    | (y>=x && y<=z) || (y<=x && y<=z) = y
+medioTripla :: (Int,Int,Int) -> Int
+medioTripla (x,y,z) | (maxTripla (x,y,z)==y && minTripla (x,y,z)==z) || (maxTripla (x,y,z)==z && minTripla (x,y,z)==y) || (x==y && x<z) || (x==z && x<y) = x
+                    | (maxTripla (x,y,z)==x && minTripla (x,y,z)==z) || (maxTripla (x,y,z)==z && minTripla (x,y,z)==x) || (y==x && y<z) || (y==z && x<y) = y
                     | otherwise = z
-
-
--- Pueden agregan las funciones que consideren necesarias
