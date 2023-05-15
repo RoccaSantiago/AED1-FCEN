24347d9ad6cb14127ef7bee41e723eb9d8b5ba44
SantiagoRocca <42080273+RoccaSantiago@users.noreply.github.com>
2023-05-15 20:26:58 -0300

    Create Guia 7.py

---
+++ Guias/Guias 7/Guia 7.py
@@ -0,0 +1,41 @@
+from math import *
+
+ 
+ #1)
+ 
+def raizDe2(x:int):
+    return round(sqrt(x),4) 
+
+#2)
+
+def imprimir_hola():
+    print("hola")
+    
+#3)
+
+def imprimir_un_verso():
+    print("He never really looks at me /n I give him every opportunity /n In the room downstairs /n He sat and stared /n In the room downstairs /n He sat and stared2 /n Ill never make that mistake again !") 
+
+#4)
+
+def factorialDe2():
+    return 2
+    
+#5)
+
+def factorialDe4():
+    return 4*3*2
+
+#6)
+
+def factorialDe5():
+    return 5*factorialDe4()
+    
+#Ejercicio 2)
+
+#1)
+
+def imprimir_saludo(nombre:str):
+    print("Hola",nombre)
+    
+#2)
