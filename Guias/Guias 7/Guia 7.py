2ed65e1589cfef553d3b38b4f10225ec721a1cb4
Santiago <Santiagorocca17@gmail.com>
2023-05-21 16:02:25 -0300

    aa

--- Guias/Guias 7/Guia 7.py
+++ Guias/Guias 7/Guia 7.py
@@ -1,10 +1,6 @@
-from math import *
-
- 
  #1)
- 
 def raizDe2(x:int):
-    return round(sqrt(x),4) 
+    return round(x**0.5,4) 
 
 #2)
 
@@ -39,3 +35,65 @@ def imprimir_saludo(nombre:str):
     print("Hola",nombre)
     
 #2)
+
+def raiz_cuadrada_de(numero:int):
+    return numero**0.5
+
+#3)
+
+def imprimir_dos_veces(estribillo:str):
+    print(estribillo * 2)
+
+#4)
+
+def es_multiplo_de(n,m:int):
+    return n%m==0
+
+#5)
+
+def es_par(numero:int):
+    return es_multiplo_de(numero,2)
+
+#6)
+
+def cantidad_de_pizzas(comensales,min_cant_de_porciones:int):
+    return round((min_cant_de_porciones*comensales)/8)
+
+#Ejercicio 3)
+
+#1)
+
+def alguno_es_0(numero1,numero2:int):
+    if numero1==0 or numero2==0:
+        return True
+    else:
+        return False 
+    
+#2)
+
+def ambos_son_0(numero1,numero2):
+    if numero1==0 and numero2==0:
+        return True
+    else:
+        return False
+
+#3)
+
+def es_nombre_largo(nombre:str):
+    if len(nombre)>=3 and len(nombre)<=8:
+        return True
+    else:
+        return False
+
+#4)
+
+def es_bisiesto(año):
+    if año%400==0 or (año%4==0 and not(año%100==0)):
+        return True 
+    else: 
+        return False
+
+#Ejercicio 4
+
+#1
+
